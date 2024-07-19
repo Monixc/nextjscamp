b2550f8adeb981b26a281fb7cb098bf2fc92e23f
Monixc <chris13404@gmail.com>
2024-07-19 14:12:41 +0900

    5일차: 실습 강사님 코드

--- 05/counter.js
+++ 05/counter.js
@@ -1,13 +1,30 @@
-const buttonPlus = document.querySelector("#plus");
-const buttonMinus = document.querySelector("#minus");
-const count = document.querySelector("h1");
+// const buttonPlus = document.querySelector("#plus");
+// const buttonMinus = document.querySelector("#minus");
+// const count = document.querySelector("h1");
 
-buttonPlus.addEventListener("click", function () {
-  count.textContent = parseInt(count.textContent) + 1;
-  console.log("+1");
+// buttonPlus.addEventListener("click", function () {
+//   count.textContent = parseInt(count.textContent) + 1;
+//   console.log("+1");
+// });
+
+// buttonMinus.addEventListener("click", function () {
+//   count.textContent = parseInt(count.textContent) - 1;
+//   console.log("-1");
+// });
+
+//강사님 코드
+let i = 0;
+const h1El = document.querySelector("h1");
+const decrementBtn = document.querySelector("button:nth-of-type(1)");
+const incrementBtn = document.querySelector("button:nth-of-type(2)");
+
+console.log(h1El, decrementBtn, incrementBtn);
+
+decrementBtn.addEventListener("click", () => {
+  console.log("decrement");
+  if (i > 0) h1El.innerText = --i;
 });
-
-buttonMinus.addEventListener("click", function () {
-  count.textContent = parseInt(count.textContent) - 1;
-  console.log("-1");
+incrementBtn.addEventListener("click", () => {
+  console.log("increment");
+  if (i < 10) h1El.innerText = ++i; //innerText는 문자열로 저장되어야 한다.
 });
