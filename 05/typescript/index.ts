2ddaea10ccac3e29a1e11d0732a8b1f8531f8622
Monixc <chris13404@gmail.com>
2024-07-19 17:35:56 +0900

    5일차: 타입스크립트 개념 정리

--- 05/typescript/index.ts
+++ 05/typescript/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, World");
+//기본 자료형
+//숫자, 문자, 논리형, undefined, null
+
+const num: number = 10;
+const str: string = "Hello";
+const bool: boolean = true;
+const und: undefined = undefined;
+const nul: null = null;
+
+//참조 자료형
+const obj: object = {};
+const arr: [] = [];
+const fun: Function = () => {};
