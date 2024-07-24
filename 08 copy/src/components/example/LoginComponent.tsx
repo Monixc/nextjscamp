b480de7cca2ebd0ec5690c14641d025d2ca4ac0d
Monixc <chris13404@gmail.com>
2024-07-24 15:58:04 +0900

    8일차: useRef

--- 08 copy/src/components/example/LoginComponent.tsx
+++ 08 copy/src/components/example/LoginComponent.tsx
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
-import Button from "../html/Button";
-import Checkbox from "../html/Checkbox";
+import { useRef, useState } from "react";
 import Input from "../html/Input";
+import Checkbox from "../html/Checkbox";
+import Button from "../html/Button";
 
 const LoginComponent = () => {
-  const [type, setType] = useState("Sign");
+  const [type, setType] = useState("Sign"); // Sign or Login
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [agree, setAgree] = useState(false);
+  const nameEl = useRef<HTMLInputElement>(null);
+  const emailEl = useRef<HTMLInputElement>(null);
+  const passwordEl = useRef<HTMLInputElement>(null);
 
   const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (email.trim() === "") {
+      alert("이메일을 입력해주세요");
+      emailEl.current?.focus();
+      return;
+    }
+
+    if (password.trim() === "") {
+      alert("비밀번호를 입력해주세요");
+      passwordEl.current?.focus();
+      return;
+    }
 
     const loginValid = email.trim() === "" || password.trim() === "" || !agree;
     const signValid = loginValid || name.trim() === "";
@@ -23,64 +38,65 @@ const LoginComponent = () => {
 
     alert(type === "Sign" ? "Signup Success" : "Login Success");
   };
-
   return (
-    <div className="item-middle bg-black">
-      <div className="w-[375px] bg-white py-10 px-[25px] inter">
-        <h1 className="text-xl font-bold text-[#4f4f4f] mb-[10px]">
-          {type}
-          Into App
-        </h1>
-        <p className="text-sm text-[#4f4f4f] mb-5">
-          Please enter your details to continue
-        </p>
-        <form
-          action=""
-          className="flex flex-col gap-4"
-          onSubmit={onSubmitHandler}>
-          {type === "Sign" && (
+    <>
+      <div className="item-middle bg-black">
+        <div className="w-[375px] bg-white py-10 px-[25px] inter">
+          <h1 className="text-xl font-bold text-[#4f4f4f] mb-[10px]">
+            {type} Into App
+          </h1>
+          <p className="text-sm text-[#4F4F4F] mb-5">
+            Please enter your details to continue.
+          </p>
+          <form className="flex flex-col gap-4" onSubmit={onSubmitHandler}>
+            {type === "Sign" && (
+              <Input
+                ref={nameEl}
+                type="text"
+                placeholder="Enter Your Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            )}
             <Input
-              type="text"
-              placeholder="Enter your name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              ref={emailEl}
+              type="email"
+              placeholder="someone@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-          )}{" "}
-          <Input
-            type="email"
-            placeholder="someone@example.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type="password"
-            placeholder="Enter Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Checkbox checked={agree} onToggle={() => setAgree((prev) => !prev)}>
-            I agree with terms and policies
-          </Checkbox>
-          <div className="flex flex-col gap-4 mt-[32px]">
-            <Button type="submit" className="bg-[#4f4f4f]">
-              {type === "Sign" ? "Sign In" : "Log In"}
-              Sign In
-            </Button>
-            <Button
-              type="button"
-              className="border border-[#4f4f4f] text-[#4f4f4f]"
-              onClick={() => {
-                setName("");
-                setEmail("");
-                setPassword("");
-                setType((prev) => (prev === "Login" ? "Signin" : "Login"));
-              }}>
-              Go To Log In
-            </Button>
-          </div>
-        </form>
+            <Input
+              ref={passwordEl}
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Checkbox
+              checked={agree}
+              onToggle={() => setAgree((prev) => !prev)}>
+              I agree with terms and policies.
+            </Checkbox>
+            <div className="flex flex-col gap-4 mt-4">
+              <Button type="submit" className="bg-[#4F4F4F]">
+                {type === "Sign" ? "Sign In" : "Log In"}
+              </Button>
+              <Button
+                type="button"
+                className="border border-[#4F4F4F] text-[#4f4f4f]"
+                onClick={() => {
+                  setName("");
+                  setEmail("");
+                  setPassword("");
+                  setType((prev) => (prev === "Login" ? "Sign" : "Login"));
+                }}>
+                Go To Log In
+              </Button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default LoginComponent;
--- 08 copy/src/components/html/Input.tsx
+++ 08 copy/src/components/html/Input.tsx
@@ -1,18 +1,23 @@
+import React, { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-type TInputProps = React.ComponentPropsWithoutRef<"input">;
+type TInputProps = React.ComponentPropsWithRef<"input">;
 
-const Input = ({ className, ...rest }: TInputProps) => {
-  return (
-    <>
+const Input = forwardRef<HTMLInputElement, TInputProps>(
+  ({ className, ...rest }, ref) => {
+    return (
       <input
+        ref={ref}
         className={twMerge(
-          `w-[full] h-[44px] rounded-lg placeholder:text-[#acacac] border border-[#4F4F4F] py-[13.5px] px-[16px] text-sm outline-none`,
+          `w-full h-[44px] rounded-lg placeholder:text-[#acacac] border border-[#4F4F4F] py-[13.5px] px-[16px] text-sm outline-none`,
           className
         )}
         {...rest}
       />
-    </>
-  );
-};
+    );
+  }
+);
+
+Input.displayName = "Input"; // Optionally set a display name for the component
+
 export default Input;
