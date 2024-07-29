7b156053a193f3f48b9c829f8167c2639fcb8d47
Monixc <chris13404@gmail.com>
2024-07-29 16:49:05 +0900

    11일차: use client

--- 11-1/src/app/page.tsx
+++ 11-1/src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { useState } from "react";
+
 export default function Home() {
-  return <h1>Next.js</h1>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  return (
+    <>
+      <h1>count: {count}</h1>
+      <button onClick={decrement}>감소</button>
+      <button onClick={increment}>증가</button>
+    </>
+  );
 }
